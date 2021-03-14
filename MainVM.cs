e70028c561532491845273539dc99b2b361ab903
kahtanov <Zop859@mail.ru>
2021-03-14 17:03:47 +0300

    наследование от DiplomaDataDataContext

--- MainVM.cs
+++ MainVM.cs
@@ -9,18 +9,87 @@ using Microsoft.Office.Interop.Word;
 using System.Collections.ObjectModel;
 using DiplomaData.Отчёты;
 using System.Windows.Input;
+using DiplomaData.Model;
+using System.Data.Linq;
 
 namespace DiplomaData
 {
-    class MainVM : peremlog
+    public static class Extension
+    {
+        public static bool SetAdd<T>(this ObservableCollection<T> ts,T item)
+        {
+            if (ts.Contains(item)) return false;
+            ts.Add(item);
+            return true;
+        }
+    }
+
+
+
+    class MainVM : peremlog, IDisposable
     {
         public lamdaCommand OpenWordTemplate { get; }
+
+
+        #region DiplomaData
+        private DiplomaDataDataContext Data;
+        /// <summary>Подключение к базе данных</summary>
+        public DiplomaDataDataContext DiplomaData { get => Data; set =>Set(ref Data ,value); }
+        #endregion
+
+
+        #region TabThesis
+        private TabTable tabThesis;
+        /// <summary>Вкладка тем</summary>
+        public TabTable TabThesis { get => tabThesis; set =>Set(ref tabThesis ,value); }
+        #endregion
+
+
+        #region Tabs
+        private ObservableCollection<Tab> tabs;
+        /// <summary>Вкладки</summary>
+        public ObservableCollection<Tab> Tabs { get => tabs; set =>Set(ref tabs ,value); }
+        #endregion
+
+        public lamdaCommand UpdateData { get; }
+        
+        public lamdaCommand AddTabThesis { get; }
 
 
         public MainVM()
         {
             OpenWordTemplate = new lamdaCommand(OnOpenWordTemplate);
-            
+
+            DiplomaData = new DiplomaDataDataContext();
+
+            TabThesis =new TabTable(DiplomaData.Thesis);
+
+            UpdateData = new lamdaCommand(DiplomaData.SubmitChanges);
+
+            Tabs = new ObservableCollection<Tab>();
+            Tabs.CollectionChanged += Tabs_CollectionChanged;
+
+            AddTabThesis = new lamdaCommand(() => Tabs.SetAdd(TabThesis));
+
+
+        }
+
+
+
+        private void Tabs_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                    foreach (Tab item in e.NewItems)
+                    {
+                        item.AClose = a => Tabs.Remove(a);
+                    }
+
+                    break;
+                default:
+                    break;
+            }
         }
 
         private void OnOpenWordTemplate()
@@ -51,6 +120,12 @@ namespace DiplomaData
         {
             foreach (ContentControl item in contentControls)
                 yield return new UContentControl(item);
+        }
+
+        public void Dispose()
+        {
+            Tabs.CollectionChanged -= Tabs_CollectionChanged;
+            DiplomaData.Dispose();
         }
     }
 
--- импорт/ImportVM.cs
+++ импорт/ImportVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,19 @@ namespace DiplomaData.импорт
 {
     public class ImportVM : peremlog
     {
+
+        #region Table
+        private Table<object> table;
+        /// <summary>таблица данных</summary>
+        public Table<object> Table { get => table; set => Set(ref table ,value); }
+        #endregion
+
+
+        #region Select
+        private object select;
+        /// <summary>выбранные табличные банные</summary>
+        public object Select { get => select; set =>Set(ref select ,value); }
+        #endregion
 
     }
 }
